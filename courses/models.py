be5fe1006c78a05bf8f1c031ecf22990ed92cc19
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2024-12-26 12:49:09 +0300

    admin paneli olusturuldu.

--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank = False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",null=False,unique=True,db_index=True)  # null olmasin ve default bos gelsin
+    slug = models.SlugField(default="",null=False,blank=True,editable=False,unique=True,db_index=True)  # null olmasin ve default bos gelsin
 
     """
     blank=False ise ve kullanıcı bir formda bu alanı doldurmazsa, Django bir ValidationError fırlatır.
@@ -29,3 +29,6 @@ blank=True, null=True: Hem formda boş bırakılabilir, hem de veritabanında NU
 class Category(models.Model):
     name = models.CharField(max_length=40)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
