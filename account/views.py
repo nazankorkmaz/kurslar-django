1dc854f9f5ae102ded6d6b45f888322bda7c78f9
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-05 13:27:19 +0300

    user login islemi eklendi.

--- account/views.py
+++ account/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import redirect, render
 
+from django.contrib.auth import authenticate,login
 # Create your views here.
 
 def user_login(request):
-    return render(request,"account/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request,username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request,"account/login.html",{"error":"username ya da parola yanlış"})
+    
+    else:
+        return render(request,"account/login.html")
+            
+        
 
 def user_register(request):
     return render(request,"account/register.html")
