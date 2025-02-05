670c0b66214f8a6db2212f563f0c7ee697f1a0c5
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-05 15:50:35 +0300

    user register islemi eklendi.

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 
 from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def user_login(request):
@@ -26,7 +28,31 @@ def user_login(request):
         
 
 def user_register(request):
-    return render(request,"account/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password != repassword:
+            return render(request, "account/register.html",
+                        {
+                            "error":"parola eşleşmiyor.",
+
+                            "username": username,
+                            "email":email
+                        })
+        if User.objects.filter(username=username).exists():
+            return render(request,"account/register.html",{"error":"username kullanıyor."})
+        if User.objects.filter(email=email).exists():
+            return render(request,"account/register.html",{"error":"email kullanıyor."})
+    
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return redirect("login")
+
+    else:
+        return render(request,"account/register.html")
 
 def user_logout(request):
     logout(request)
