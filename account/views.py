7109e01a28fe60ec2d27783fc392ad6e8809a391
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-05 12:50:20 +0300

    account uygulaması eklendi ve navbar responsive olarak duzeltildi.

---
+++ account/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect, render
+
+# Create your views here.
+
+def user_login(request):
+    return render(request,"account/login.html")
+
+def user_register(request):
+    return render(request,"account/register.html")
+
+def user_logout(request):
+    return redirect("index")
--- courseapp/urls.py
+++ courseapp/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('',include('pages.urls')),
     path('kurs/',include('courses.urls')),
+    path('account/',include('account.urls')),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
