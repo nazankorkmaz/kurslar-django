f9157e6821ed1ecc3c2c04c700e262a3fdfcfeec
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-07 17:23:49 +0300

    envirenmont variables

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-698h(r3ofy*e@1b1wvh2@^)5m%%z#$mxp_q&@qmy$8$#dojidv'
+
+#SECRET_KEY = 'django-insecure-698h(r3ofy*e@1b1wvh2@^)5m%%z#$mxp_q&@qmy$8$#dojidv'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT",True)#True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+    ]# []
 
 
 # Application definition
@@ -110,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'TR'
 
 TIME_ZONE = 'UTC'
 
